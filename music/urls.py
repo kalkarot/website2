6b2ad514d6bffd53ed321a41a8439ceb7932e8c5
samuel <samueler2013@gmail.com>
2018-04-25 10:45:06 +0000

    saved1

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    #/music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
